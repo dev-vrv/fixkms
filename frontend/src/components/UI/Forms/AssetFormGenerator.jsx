f8c2ac96ac5c6b2cceb366e6b55c59b4d0030fea
Roman <dev.vrv@gmail.com>
2025-03-14 22:18:59 -0700

    Удалены неиспользуемые файлы сборки фронтенда, оптимизирован импорт компонентов в AssetFormGenerator.

--- frontend/src/components/UI/Forms/AssetFormGenerator.jsx
+++ frontend/src/components/UI/Forms/AssetFormGenerator.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, use } from "react";
+import React, { useState, useEffect } from "react";
 import { fetchForm } from "../../../utils/fetchData";
 import MyButton from "../Button/MyButton";
 import SelectField from "../Select/Select";
